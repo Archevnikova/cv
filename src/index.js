8764d22c499572fb8467d88aa289f2506f349c90
Archevnikova <ms.orlovakatya@mail.ru>
2024-04-23 18:28:58 +0300

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./img/avatar.png" ;
+import "./style/style.scss";
+import "./style/reset.scss";
+import "./style/link.scss";
+import "./style/header/header.scss";
+import "./style/about/about.scss";
+import "./style/projects/projects.scss";
