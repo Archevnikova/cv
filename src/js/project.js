5ebe08887802d850da0d667d245e53f9408ef67b
Archevnikova <ms.orlovakatya@mail.ru>
2024-07-09 18:29:19 +0300

    feat: modification

--- src/js/project.js
+++ src/js/project.js
@@ -4,7 +4,8 @@ const cardHeader = document.querySelector(".projects__card-header");
 const cardImg = document.querySelector(".projects__card-img");
 const cardDescription = document.querySelector(".projects__card-description");
 const linkBtn = document.querySelector(".projects__card-git");
-const projectBtn = document.querySelectorAll(".projects__btn > .projects__btn-link");
+const linkGitPages =document.querySelector(".projects__card-git-pages");
+// const projectBtn = document.querySelectorAll(".projects__btn > .projects__btn-link");
 
 
 calcBtn.onclick = function(){ 
@@ -12,14 +13,15 @@ calcBtn.onclick = function(){
     cardImg.src = "./img/calc2.png";
     cardDescription.textContent = "Made using Jquery HTML CSS JavaScript";
     linkBtn.href = "https://github.com/Archevnikova/-iphone-calculator";
-    for (let i = 0; i < projectBtn.length; i++){
-        projectBtn[i].style.backgroundColor = "#21262b";
-    }
+    linkGitPages.href = "https://archevnikova.github.io/-iphone-calculator";
+    // for (let i = 0; i < projectBtn.length; i++){
+    //     projectBtn[i].style.backgroundColor = "#21262b";
+    // }
 
     
-    for (let i = 0; i < projectBtn.length; i++){
-        projectBtn[i].style.backgroundColor = "#7733FF";
-    }
+    // for (let i = 0; i < projectBtn.length; i++){
+    //     projectBtn[i].style.backgroundColor = "#7733FF";
+    // }
 
 };
 cosmoBtn.onclick = function(){
@@ -27,15 +29,16 @@ cosmoBtn.onclick = function(){
     cardImg.src = "./img/cosmotur.png";
     cardDescription.textContent = "Only HTML CSS";
     linkBtn.href= "https://github.com/Archevnikova/cosmo";
+    linkGitPages.href = "https://archevnikova.github.io/cosmo";
 
 
-    for (let i = 0; i < projectBtn.length; i++){
-        projectBtn[i].style.backgroundColor = "#21262b";
-    }
+    // for (let i = 0; i < projectBtn.length; i++){
+    //     projectBtn[i].style.backgroundColor = "#21262b";
+    // }
 
-    for (let i = 0; i <= projectBtn.length; i++){
-        if(i >= 2 ) break;
-        projectBtn[i].style.backgroundColor = "#7733FF";
-    }
+    // for (let i = 0; i <= projectBtn.length; i++){
+    //     if(i >= 2 ) break;
+    //     projectBtn[i].style.backgroundColor = "#7733FF";
+    // }
     
 }
